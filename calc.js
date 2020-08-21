9f2a6d5537722baedc3c63f0dc229742cda3fe01
Denwerpr <denwerpr666@gmail.com>
2020-08-21 16:57:39 +0300

    Добавил пример JS файл

---
+++ calc.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function ($) {
+	$("#form_calc").change(function() {
+		
+		var totalPrice   = 0,
+            values       = [];
+        
+		$('input[type=checkbox], input[type=radio]').each( function() {
+            if( $(this).is(':checked') ) {
+                values.push($(this).val());
+				totalPrice += parseInt($(this).val());
+            }
+        });
+
+		$("#price span").text(totalPrice);	
+		
+	});	
+});
+
